3ed68de2fb26e192325ba75c4d62a99f1b16923c
codekim97 <kimtaeu4616@gmail.com>
2022-07-11 14:15:21 +0900

    template might not exist or might not be accessible by any of the configured Template Resolvers 에러 해결 by enable in application.properties

---
+++ spring-mvc/src/main/java/spring/mvc/realmvc/structure_1/StructureMemberFormController.java
@@ -0,0 +1,15 @@
+package spring.mvc.realmvc.structure_1;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StructureMemberFormController{
+    @RequestMapping("/realmvc/structure_1/form")
+    public ModelAndView process() {
+        System.out.println("StructureMemberFormController.process");
+        return new ModelAndView("new-form");
+    }
+}
