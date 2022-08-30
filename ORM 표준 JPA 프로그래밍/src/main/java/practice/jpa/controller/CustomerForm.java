6c9e8b677cca1ecc4770051e6d5ce46ac07b5bbc
codekim97 <kimtaeu4616@gmail.com>
2022-08-30 10:33:27 +0900

    JPA 실습 - 회원 가입 Valid 추가

--- ORM 표준 JPA 프로그래밍/src/main/java/practice/jpa/controller/CustomerForm.java
+++ ORM 표준 JPA 프로그래밍/src/main/java/practice/jpa/controller/CustomerForm.java
@@ -8,10 +8,15 @@ import javax.validation.constraints.NotEmpty;
 @Setter @Getter
 public class CustomerForm {
 
-    @NotEmpty
+    @NotEmpty(message = "이름을 입력해주세요")
     private String name;
 
+    @NotEmpty(message = "도를 입력해주세요")
     private String palDo;
+
+    @NotEmpty(message = "동을 입력해주세요")
     private String dong;
+
+    @NotEmpty(message = "도로를 입력해주세요")
     private String street;
 }
