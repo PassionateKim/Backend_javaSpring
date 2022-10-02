f0df45a776ecc623839de9e5b0ef0026eeeccf87
codekim97 <kimtaeu4616@gmail.com>
2022-10-02 11:24:34 +0900

    스프링 시큐리티 적용 시 기존 테스트 코드에서 문제 해결

--- 스프링 부트와 AWS로 혼자 구현하는 웹_서비스/src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹_서비스/src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
 
 @RunWith(SpringRunner.class)
@@ -24,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        Assertions.assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        Assertions.assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
